8eafa95153aa755be6868ddda3669c55be991be4
Andrews Annan <68544288+AndyAnnan@users.noreply.github.com>
2020-08-20 16:40:48 -1200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Annan from './week-3/Andy.js';
+
+import './week-3/css/components.css';
+import './week-3/css/global.css';
+import './week-3/css/layout.css';
+import './week-3/css/sample-wallpaper.css';
+
+function App() {
+  return (
+    <div className="App">
+
+     <Annan/>
+      </div>
+  );
+}
+
+export default App;
